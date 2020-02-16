d6c345340d46fa1b8448c1fab8c7a57137622ea6
lpdft <772346730@qq.com>
2020-02-16 23:34:41 +0800

    [ruyue] feat: input number and judge is it normative

--- src/main/java/com/thoughtworks/CardMachine.java
+++ src/main/java/com/thoughtworks/CardMachine.java
@@ -1,4 +1,24 @@
 package com.thoughtworks;
 
+
+import java.util.Scanner;
+
 public class CardMachine {
+
+    public int getInput() {
+        boolean isTrue = false;
+        int n = 0;
+        while(!isTrue) {
+            System.out.print("请输入抽取牌的数量：");
+            Scanner input = new Scanner(System.in);
+            n = input.nextInt();
+            if(n < 1 || n > 52) {
+                System.out.println("Out of range, please enter again.");
+            } else {
+                isTrue = true;
+            }
+        }
+        return n;
+    }
 }
+
