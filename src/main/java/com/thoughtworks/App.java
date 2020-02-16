d9b7008e8e6856b20e050c731b782965c9568c43
lpdft <772346730@qq.com>
2020-02-16 23:08:21 +0800

    [ruyue] feat: initialize all card

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -1,7 +1,23 @@
 package com.thoughtworks;
 
+import java.util.ArrayList;
+
 public class App {
 
   public static void main(String[] args) {
+      Card card = new Card(createCard());
+
+  }
+
+  public static ArrayList<String> createCard() {
+    String[] pattern = {"黑桃","红桃","梅花","方片"};
+    String[] number = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    ArrayList<String> card = new ArrayList<>();
+    for (String s : pattern) {
+      for (String value : number) {
+        card.add(s + " " + value);
+      }
+    }
+    return card;
   }
 }
--- src/main/java/com/thoughtworks/Card.java
+++ src/main/java/com/thoughtworks/Card.java
@@ -1,4 +1,19 @@
 package com.thoughtworks;
 
+import java.util.ArrayList;
+
 public class Card {
+    private ArrayList<String> card = new ArrayList<>();
+
+    public Card(ArrayList<String> card) {
+        this.card = card;
+    }
+
+    public ArrayList<String> getCard() {
+        return card;
+    }
+
+    public void setCard(ArrayList<String> card) {
+        this.card = card;
+    }
 }
